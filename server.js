5443a7bbd8de84ebf6f977d370f5ec498077a3d2
Santiago Alessandro <saantialessa@gmail.com>
2021-03-02 21:46:52 -0300

    2nd attempt to dply

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config()
 const cors = require('cors')
-const router = require('./router/index')
+const router = require('./routes/index')
 require('./config/database')
 
 const app = express()
