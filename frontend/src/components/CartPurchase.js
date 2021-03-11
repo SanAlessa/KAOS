17a367c218a13efb9cc1f845eff646a26f84e537
lucascachazo <luki_89@hotmail.com>
2021-03-10 22:44:56 -0300

    responsive

--- frontend/src/components/CartPurchase.js
+++ frontend/src/components/CartPurchase.js
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux'
 import CartCards from './CartCards'
+import {BiPurchaseTagAlt} from 'react-icons/bi'
 
 const CartPurchase = ({products, deleteProduct, checkout}) => {
     console.log(checkout)
     return(
         <>
             <div className='containerCart'>
-                { products && products.length === 0 ? <p>Empezá a comprar</p> :
+                { products && products.length === 0 ? <h6>Empezá a comprar</h6> :
                 <>
-                <h2>Tu compra</h2>
+                <h6 className="titulo3"><BiPurchaseTagAlt/> TU COMPRA</h6>
                 <div className ='containerPurchase'>
                     {products &&  products.length > 0 && products.map(product => <CartCards key={product.id} product={product} />)}
                 </div>
